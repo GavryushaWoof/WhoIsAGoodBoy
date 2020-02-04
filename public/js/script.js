928db7901be1f0d99be7b49a625e5a010436024c
GavryushaWoof <gavgav.cat@yandex.ru>
2020-02-04 17:05:15 +0300

    first

---
+++ public/js/script.js
@@ -0,0 +1,65 @@
+window.dogs = [{
+        "like": 5,
+        "src": "../img/dog/dog1.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog2.jpg"
+    },
+    {
+        "like": 1,
+        "src": "../img/dog/dog3.png"
+    },
+    {
+        "like": 25,
+        "src": "../img/dog/dog4.jpg"
+    },
+    {
+        "like": 6,
+        "src": "../img/dog/dog5.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog6.jpg"
+    },
+    {
+        "like": 10,
+        "src": "../img/dog/dog7.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog8.jpg"
+    },
+    {
+        "like": 15,
+        "src": "../img/dog/dog9.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog10.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog11.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog12.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog13.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog14.jpg"
+    },
+    {
+        "like": 0,
+        "src": "../img/dog/dog15.jpg"
+    },
+    {
+        "like": 50,
+        "src": "../img/dog/dog16.png"
+    },
+]
